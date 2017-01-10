a360f6eca14466ef12529fcd866b5b1c76331ce4
Madison Kerndt <madison.kerndt@gmail.com>
2017-01-10 12:10:02 -0700

    mpersist data in the DB & Ember Data

--- app/components/add-form.js
+++ app/components/add-form.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    addPerson: function() {
+      let newPerson = {
+        name: this.get('name'),
+        description: this.get('description')
+      };
+      this.sendAction('createAction', newPerson);
+      this.set('name', '');
+      this.set('description', '');
+    }
+  }
 });
